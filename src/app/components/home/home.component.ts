4109ad0cbe3026b8066b3d7065796d645016f55b
iagosm <iagosousa201486@gmail.com>
2024-01-21 22:47:15 -0300

    Inicialização do projeto

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    NgOptimizedImage,
+    BtnPrimaryComponent
+  ],
+  providers: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
