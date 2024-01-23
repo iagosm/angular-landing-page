6a700fd7bf4de033ac1fb50dd2b67dea56c137a5
iagosm <iagosousa201486@gmail.com>
2024-01-22 22:32:50 -0300

    Finalização do newsletterForm

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,8 @@ import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
   imports: [
     HeaderComponent,
     NgOptimizedImage,
-    BtnPrimaryComponent
+    BtnPrimaryComponent,
+    NewsletterFormComponent
   ],
   providers: [],
   templateUrl: './home.component.html',
